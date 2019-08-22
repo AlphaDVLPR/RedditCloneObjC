7174a8687a5d8b451c0b85d1ded725d2edda4286
AlphaDVLPR <alphadvlpr@AlphaDVLPRs-MacBook-Pro.local>
2019-08-22 14:17:26 -0600

    This is the starting point for the Reddit Clone App

---
+++ RedditObjC/RedditObjC/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedditObjC
+//
+//  Created by AlphaDVLPR on 8/22/19.
+//  Copyright © 2019 AlphaDVLPR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
